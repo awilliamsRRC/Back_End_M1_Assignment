017b53da031efae3486b798a670fe94b69c0ef57
rrc_F2024FA_awilliams <awilliams3@academic.rrc.ca>
2025-01-20 13:47:00 -0600

    Completed app.ts for Part 2

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
-import express, { Express } from "express";
-
+// Import from express
+import express, {Express,Request,Response} from "express";
 // Initialize Express application
 const app: Express = express();
-
 // Define a route
-app.get("/", (req, res) => {
-	res.send("Hello, World!");
+app.get("/",(req:Request,res:Response) => {
+    res.send("Hello, World!");
+
 });
 
 export default app;
